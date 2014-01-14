8d6f2156a3ddd3e79fcfe473e05b33b4ffb086dc
Jay Patel <jayhp9@stanford.edu>
2014-01-13 22:42:57 -0800

    testing js path again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,3 @@
 //= require turbolinks
 //= require_tree ./bootstrap
 //= require_tree ./authoring
-
-// require_tree ./sitewide
-// require twitter/bootstrap
-// require d3
-// require bootstrap/js/bootstrap
-
-
-// $(document).ready(timelineReady);
-// $(document).on('page:load', timelineReady);
